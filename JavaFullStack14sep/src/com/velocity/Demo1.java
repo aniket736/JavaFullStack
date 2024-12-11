bfd42e34ec0ce59e68fa2ca89b035af566d30a16
aniket <aniketjoshi113@gmail.com>
2024-12-11 17:41:28 +0530

    arraylist of shop added

---
+++ JavaFullStack14sep/src/com/velocity/Demo1.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		List list=new ArrayList();
+		list.add("madan");
+		list.add("piche");
+		System.out.println(list);
+		
+	}
+
+}
